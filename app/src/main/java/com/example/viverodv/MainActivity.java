943fde4e7b63e2eeffabd4eeb1af57e949d058b7
AgUsBF <agustinbeceyro@gmail.com>
2025-10-08 19:35:11 -0300

    feat: agregar funcionalidad preliminar al boton del formulario

--- app/src/main/java/com/example/viverodv/MainActivity.java
+++ app/src/main/java/com/example/viverodv/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.viverodv;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,5 +26,37 @@ public class MainActivity extends AppCompatActivity {
         editTextPlantType = findViewById(R.id.editTextPlantType);
         editTextPlantPrice = findViewById(R.id.editTextPlantPrice);
         buttonAddProduct = findViewById(R.id.buttonAddProduct);
+
+        // Listener del boton
+        buttonAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addProduct();
+            }
+        });
+    }
+
+    // Validar y cargar a la lista
+    private void addProduct() {
+        // Obtener data de los campos
+        String name = editTextPlantName.getText().toString().trim();
+        String type = editTextPlantType.getText().toString().trim();
+        String price = editTextPlantPrice.getText().toString().trim();
+
+        Log.e("MainActivity", "Nombre: " + name + ", Tipo: " + type + ", Precio: " + price);
+
+        // Verificar si estan vacios
+        if (TextUtils.isEmpty(name) || TextUtils.isEmpty(type) || TextUtils.isEmpty(price)) {
+            //TODO: Generar advertencia al usuario
+            return;
+        }
+        
+        // TODO: Agrega la tarjeta del producto a la lista
+        
+        // Limpia los campos y enfoca el primero
+        editTextPlantName.setText("");
+        editTextPlantType.setText("");
+        editTextPlantPrice.setText("");
+        editTextPlantName.requestFocus();
     }
 }
